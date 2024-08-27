a38fa0e82b425d78c8e5bba42e3ce1cf83c9a404
Sottili <felipesottili32@gmail.com>
2024-08-27 19:19:19 -0300

    feat: Create service of database and initialize in index and add postgres dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+
 import dotenv from "dotenv";
+
+import { db } from "./services/database";
 
 dotenv.config();
 
+const database = new db();
+database.initialize();
+
 const app = express();
-const port = 80;
+const port = 81;
 
 app.listen(port, () => {
   console.log(`App running in port ${port}`);
